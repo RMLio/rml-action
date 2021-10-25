d7ea7ec8eb80492605e43df7fe6f0aadb9295f96
elytvyno <elena.lytvynova@ugent.be>
2021-10-25 10:56:47 +0200

    feat: added some additional comments and removed logging

--- converter.sh
+++ converter.sh
@@ -2,19 +2,19 @@
 
 while read file
 do
-    # get a filename without extension
+    # get a filename without an extension
     filename=$(echo `basename $file` | sed -e 's/\..*//')
+	# filename for the output file containing RDF
     output_filename="${filename}_output.ttl"
-    echo $INPUTS_INPUT_DIRECTORY
     # convert YARRRML rules to RML
     yarrrml-parser -i $file -o $INPUTS_INPUT_DIRECTORY/rml_rules.rml.ttl
-    # go to the input directory
+    # go to the directory with data files
     cd $INPUTS_INPUT_DIRECTORY
     # convert RML rules to RDF
     java -jar rmlmapper.jar -m rml_rules.rml.ttl -o $output_filename -s $SERIALIZATION_FORMAT
     # go back to the root directory of a repository
     cd $WORKING_DIRECTORY
-    # copy the output file to the output directory
+    # move the output file to the output directory
     mv $INPUTS_INPUT_DIRECTORY/$output_filename $INPUTS_OUTPUT_DIRECTORY/$output_filename
     # remove the temporary file with RML rules
     rm $INPUTS_INPUT_DIRECTORY/rml_rules.rml.ttl
